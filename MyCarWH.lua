8775fb620c3e7a7a2674d019025875cb2a47ab86
SaburoShimizu <44968818+SaburoShimizu@users.noreply.github.com>
2018-11-13 18:14:30 +1000

    Update MyCarWH.lua

--- MyCarWH.lua
+++ MyCarWH.lua
@@ -1,7 +1,7 @@
 require "lib.moonloader"
 require "lib.sampfuncs"
 local SE = require 'lib.samp.events'
-script_version('1.0')
+script_version('1.1')
 
 active = 1
 actives = 0
@@ -11,6 +11,7 @@ testupdate = 1
 function main()
     if not isSampfuncsLoaded() or not isSampLoaded() then return end
     while not isSampAvailable() do wait(100) end
+	sampAddChatMessage('{FF0000}[MyCarWH] {FFFFFF}Р—Р°РіСЂСѓР¶РµРЅРѕ. Р’РµСЂСЃРёСЏ СЃРєСЂРёРїС‚Р°: {FF0000}' ..thisScript().version, -1)
     sampRegisterChatCommand('getmycar', carsuka)
     sampRegisterChatCommand('delmycar', delcar)
     sampRegisterChatCommand('getmycarwh', carwh)
@@ -28,12 +29,12 @@ function carsuka(sss)
         if active == 1 then
             for i = 1, #cars do
                 local result, id = sampGetVehicleIdByCarHandle(cars[i])
-                if id == sss then if actives == 0 then dtd = sampCreate3dText('Моё авто', 0xFFFF0000, 0, 0, 0, 99999, 0, - 1, sss) actives = 1
+                if id == sss then if actives == 0 then dtd = sampCreate3dText('РњРѕС‘ Р°РІС‚Рѕ', 0xFFFF0000, 0, 0, 0, 99999, 0, - 1, sss) actives = 1
                     print(dtd)
                     local positionX, positionY, positionZ = getCarCoordinates(cars[i])
-                    sampAddChatMessage(string.format('{FF0000}[CarHelper] {FFFFFF}Координаты машины: {FF0000}X: %d, Y: %d, Z: %d.', positionX, positionY, positionZ), - 1)
+                    sampAddChatMessage(string.format('{FF0000}[CarHelper] {FFFFFF}РљРѕРѕСЂРґРёРЅР°С‚С‹ РјР°С€РёРЅС‹: {FF0000}X: %d, Y: %d, Z: %d.', positionX, positionY, positionZ), - 1)
                 else
-                    sampAddChatMessage('{FF0000}[CarHelper] {FFFFFF}Метка уже была создана. Она была автоматически удаелна. Введите команду повторно', - 1) delcar() end
+                    sampAddChatMessage('{FF0000}[CarHelper] {FFFFFF}РњРµС‚РєР° СѓР¶Рµ Р±С‹Р»Р° СЃРѕР·РґР°РЅР°. РћРЅР° Р±С‹Р»Р° Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРё СѓРґР°РµР»РЅР°. Р’РІРµРґРёС‚Рµ РєРѕРјР°РЅРґСѓ РїРѕРІС‚РѕСЂРЅРѕ', - 1) delcar() end
                 end
             end
         end
@@ -46,12 +47,12 @@ function carwh(sss)
         if active == 1 then
             for i = 1, #cars do
                 local result, id = sampGetVehicleIdByCarHandle(cars[i])
-                if id == sss then if actives == 0 then dtd = sampCreate3dText('Моё авто', 0xFFFF0000, 0, 0, 0, 99999, 1, - 1, sss) actives = 1
+                if id == sss then if actives == 0 then dtd = sampCreate3dText('РњРѕС‘ Р°РІС‚Рѕ', 0xFFFF0000, 0, 0, 0, 99999, 1, - 1, sss) actives = 1
                     print(dtd)
                     local positionX, positionY, positionZ = getCarCoordinates(cars[i])
-                    sampAddChatMessage(string.format('{FF0000}[CarHelper] {FFFFFF}Координаты машины: {FF0000}X: %d, Y: %d, Z: %d.', positionX, positionY, positionZ), - 1)
+                    sampAddChatMessage(string.format('{FF0000}[CarHelper] {FFFFFF}РљРѕРѕСЂРґРёРЅР°С‚С‹ РјР°С€РёРЅС‹: {FF0000}X: %d, Y: %d, Z: %d.', positionX, positionY, positionZ), - 1)
                 else
-                    sampAddChatMessage('{FF0000}[CarHelper] {FFFFFF}Метка уже была создана. Она была автоматически удаелна. Введите команду повторно', - 1) delcar() end
+                    sampAddChatMessage('{FF0000}[CarHelper] {FFFFFF}РњРµС‚РєР° СѓР¶Рµ Р±С‹Р»Р° СЃРѕР·РґР°РЅР°. РћРЅР° Р±С‹Р»Р° Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРё СѓРґР°РµР»РЅР°. Р’РІРµРґРёС‚Рµ РєРѕРјР°РЅРґСѓ РїРѕРІС‚РѕСЂРЅРѕ', - 1) delcar() end
                 end
             end
         end
@@ -67,15 +68,16 @@ function update()
     lua_thread.create(function()
         patch = getWorkingDirectory() .. '\\Ver.txt'
         if testupdate == 1 then downloadUrlToFile('https://github.com/SaburoShimizu/TestScript/raw/master/Version.txt', patch, _)
-            print('скачало')
+            print('СЃРєР°С‡Р°Р»Рѕ')
 			wait(3000)
             file = io.open('moonloader\\Ver.txt', 'r')
             text = file:read('*a')
             if text:find('Ver = .+') then
                 ver = text:match('Ver = (.+)')
-                if ver > thisScript().version then sampAddChatMessage('Обнаружено обновление. Для скачивания введите /updatenah', - 1) end
+                if ver > thisScript().version then sampAddChatMessage('РћР±РЅР°СЂСѓР¶РµРЅРѕ РѕР±РЅРѕРІР»РµРЅРёРµ. Р”Р»СЏ СЃРєР°С‡РёРІР°РЅРёСЏ РІРІРµРґРёС‚Рµ /updatenah', - 1) end
             end
 			file:close()
+			os.remove(getWorkingDirectory() ..'\\Ver.txt')
         end
     end)
 end
@@ -83,8 +85,8 @@ end
 function updatenah()
     lua_thread.create(function()
         patch = getWorkingDirectory() .. '\\MyCarWH.lua'
-        downloadUrlToFile('HMMMMM', patch, _)
-            sampAddChatMessage('{FF0000}[MyCarWH]{FFFFFF} Обновление началось. Скрипт перезагрузится автоматически', -1)
+        downloadUrlToFile('https://github.com/SaburoShimizu/TestScript/raw/master/MyCarWH.lua', patch, _)
+            sampAddChatMessage('{FF0000}[MyCarWH]{FFFFFF} РћР±РЅРѕРІР»РµРЅРёРµ РЅР°С‡Р°Р»РѕСЃСЊ. {FF0000}РЎРєСЂРёРїС‚ РїРµСЂРµР·Р°РіСЂСѓР·РёС‚СЃСЏ Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРё', -1)
 			wait(3000)
 			thisScript().reload()
             end)
